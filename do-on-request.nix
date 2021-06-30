d4ff917295dd3c2a3287c661a745b2c5a2a620d1
Haf <haf@protonmail.ch>
2021-06-30 20:26:30 +0200

    reverting all changes

--- do-on-request.nix
+++ do-on-request.nix
@@ -5,66 +5,66 @@ let
 in
   {
     options.services.do-on-request = {
-      #enable = mkEnableOption "Run something when receiving a request on some port";
+      enable = mkEnableOption "Run something when receiving a request on some port";
 
-      #port = mkOption {
-        #type = types.int;
-        #default = 12222;
-        #description = "Port on which to listen";
-      #};
+      port = mkOption {
+        type = types.int;
+        default = 12222;
+        description = "Port on which to listen";
+      };
 
-      #script = mkOption {
-        #type = types.str;
-        #default = null;
-        #description = "Script to run when a request is received";
-      #};
+      script = mkOption {
+        type = types.str;
+        default = null;
+        description = "Script to run when a request is received";
+      };
 
-      #preScript = mkOption {
-        #type = types.str;
-        #default = null;
-        #description = "Script to run before starting the unit";
-      #};
+      preScript = mkOption {
+        type = types.str;
+        default = null;
+        description = "Script to run before starting the unit";
+      };
 
-      #workingDirectory = mkOption {
-        #type = types.path;
-        #default = "/";
-        #description = "Path on which to run the script";
-      #};
+      workingDirectory = mkOption {
+        type = types.path;
+        default = "/";
+        description = "Path on which to run the script";
+      };
     };
 
-    #config = mkIf cfg.enable {
-      #assertions = [
-        #{
-          #assertion = cfg.script != null;
-          #message = "services.do-on-request.script must be set!";
-        #}
-      #];
+    config = mkIf cfg.enable {
+      assertions = [
+        {
+          assertion = cfg.script != null;
+          message = "services.do-on-request.script must be set!";
+        }
+      ];
 
-      #systemd.services.do-on-request = {
-        #description = "Run a script when a request is received on port ${toString cfg.port}";
+      systemd.services.do-on-request = {
+        description = "Run a script when a request is received on port ${toString cfg.port}";
         
-        #preStart = ''
-          #mkdir ${cfg.workingDirectory} || true
-        #'' + optionalString (cfg.preScript != null) "\n ${cfg.preScript}"
-        #;
+        preStart = ''
+          mkdir ${cfg.workingDirectory} || true
+        '' + optionalString (cfg.preScript != null) "\n ${cfg.preScript}"
+        ;
 
-        #serviceConfig = {
-          #Restart = "on-failure";
-          #WorkingDirectory = "${cfg.workingDirectory}";
-        #};
+        serviceConfig = {
+          Restart = "on-failure";
+          WorkingDirectory = "${cfg.workingDirectory}";
+        };
 
-        #script = ''
-          ##!${pkgs.stdenv.shell}
-          #set -x
-          #while true;
-          #do {
-          #${cfg.script}
-          #} | ${pkgs.busybox}/bin/nc -l -p ${toString cfg.port};
-          #done
-          #'';
+        script = ''
+          #!${pkgs.stdenv.shell}
+          set -x
+          while true;
+          do {
+          ${cfg.script}
+          } | ${pkgs.busybox}/bin/nc -l -p ${toString cfg.port};
+          done
+          '';
 
-          #wantedBy = [ "default.target" ];
-      #};
-    #};
+          wantedBy = [ "default.target" ];
+      };
+    };
 
   }
