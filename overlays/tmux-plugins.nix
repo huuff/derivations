4c3644cf68c5074d5d9fd85205af35b86cd3ecbe
Haf <haf@protonmail.ch>
2021-07-10 16:12:30 +0200

    third attempt

--- overlays/tmux-plugins.nix
+++ overlays/tmux-plugins.nix
@@ -1,14 +1,14 @@
 final: prev: {
   tmuxPlugins = {
-    better-mouse-mode = prev.pkgs.tmuxPlugins.mkTmuxPlugin {
+    better-mouse-mode = prev.tmuxPlugins.mkTmuxPlugin {
       pluginName = "better-mouse-mode";
       version = "rev-aa59077c635ab21b251bd8cb4dc24c415e64a58e";
-      src = prev.pkgs.fetchFromGitHub {
+      src = prev.fetchFromGitHub {
         owner = "NHDaly";
         repo = "tmux-better-mouse-mode";
         rev = "aa59077c635ab21b251bd8cb4dc24c415e64a58e";
         sha256 = "06346ih3hzwszhkj25g4xv5av7292s6sdbrdpx39p0n3kgf5mwww";
       };
     };
-  } // prev.pkgs.tmuxPlugins;
+  } // prev.tmuxPlugins;
 }
