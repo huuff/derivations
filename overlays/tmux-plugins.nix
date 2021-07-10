d23af2bff58379f800f1aeae6d2d800c4ecd31b2
Haf <haf@protonmail.ch>
2021-07-10 16:09:25 +0200

    second attempt

--- overlays/tmux-plugins.nix
+++ overlays/tmux-plugins.nix
@@ -1,15 +1,14 @@
-{ tmuxPlugins}:
-    final: prev: {
-      tmuxPlugins = {
-      better-mouse-mode = prev.pkgs.tmuxPlugins.mkTmuxPlugin {
-        pluginName = "better-mouse-mode";
-        version = "rev-aa59077c635ab21b251bd8cb4dc24c415e64a58e";
-        src = prev.pkgs.fetchFromGitHub {
-          owner = "NHDaly";
-          repo = "tmux-better-mouse-mode";
-          rev = "aa59077c635ab21b251bd8cb4dc24c415e64a58e";
-          sha256 = "06346ih3hzwszhkj25g4xv5av7292s6sdbrdpx39p0n3kgf5mwww";
-        };
+final: prev: {
+  tmuxPlugins = {
+    better-mouse-mode = prev.pkgs.tmuxPlugins.mkTmuxPlugin {
+      pluginName = "better-mouse-mode";
+      version = "rev-aa59077c635ab21b251bd8cb4dc24c415e64a58e";
+      src = prev.pkgs.fetchFromGitHub {
+        owner = "NHDaly";
+        repo = "tmux-better-mouse-mode";
+        rev = "aa59077c635ab21b251bd8cb4dc24c415e64a58e";
+        sha256 = "06346ih3hzwszhkj25g4xv5av7292s6sdbrdpx39p0n3kgf5mwww";
       };
-    } // prev.pkgs.tmuxPlugins;
-    }
+    };
+  } // prev.pkgs.tmuxPlugins;
+}
